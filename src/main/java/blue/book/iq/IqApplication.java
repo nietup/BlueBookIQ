e50dac664f0ed3309bb60e993ecd4193edf9a27f
nietup <jakub.nietupski@gmail.com>
2022-02-13 10:47:40 +0100

    init

---
+++ src/main/java/blue/book/iq/IqApplication.java
@@ -0,0 +1,13 @@
+package blue.book.iq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/blue/book/iq/IqApplicationTests.java
@@ -0,0 +1,13 @@
+package blue.book.iq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
